71350b462640db545247e59010e03f523d192873
Nnisik <nisik0120@gmail.com>
2023-11-03 16:02:55 +0300

    added buttons on top frame + image folder with icons

--- new_ver/main.py
+++ new_ver/main.py
@@ -11,48 +11,41 @@ class Main(object):
         mainFrame.pack()
 
         #top frames
-        topFrame = Frame(
-            mainFrame, 
-            width=1350, 
-            height=70,
-            bg="#fafafa", 
-            padx=20, 
-            relief=SUNKEN,
-            borderwidth=2
-        )
+        topFrame = Frame(mainFrame, width=1350, height=70, bg="#fafafa", padx=20, relief=SUNKEN, borderwidth=2)
         topFrame.pack(side=TOP, fill=X)
 
         #center frame
-        centerFrame = Frame(
-            mainFrame,
-            width=1350,
-            relief=RIDGE,
-            bg='#e0f0f0',
-            height=680
-        )
+        centerFrame = Frame( mainFrame, width=1350, relief=RIDGE, bg='#e0f0f0', height=680)
         centerFrame.pack(side=TOP)
 
         #Center left Frame
-        centerLeftFrame = Frame(
-            centerFrame, 
-            width=900, 
-            height=700, 
-            bg='#e0f0f0',
-            borderwidth=2,
-            relief='sunken'
-        )
+        centerLeftFrame = Frame(centerFrame, width=900, height=700, bg='#e0f0f0', borderwidth=2, relief='sunken')
         centerLeftFrame.pack(side=LEFT)
 
         # center right frame
-        centerRightFrame = Frame(
-            centerFrame, 
-            width=450,
-            height=700,
-            bg='#e0f0f0',
-            borderwidth=2,
-            relief='sunken'
-        )
+        centerRightFrame = Frame(centerFrame,  width=450, height=700, bg='#e0f0f0', borderwidth=2, relief='sunken')
         centerRightFrame.pack()
+
+        #search bar
+        search_bar = LabelFrame(centerRightFrame, width=440, height=75, text="Search Box", bg="#9bc977")
+        search_bar.pack(fill=BOTH)
+
+        #list bar
+        list_bar = LabelFrame(centerRightFrame, width=440, height=175, text='List Box', bg='#fcc324')
+        list_bar.pack(fill=BOTH)
+
+        #add book
+        self.btnbook = Button(topFrame, text='Add Book', font='arial 12 bold')
+        self.btnbook.pack(side=LEFT,padx=10)
+
+        #add member button
+        self.btnmembers=Button(topFrame, text='Add Member', font='arial 12 bold', padx=10)
+        self.btnmembers.pack(side=LEFT)
+
+        #give book
+        self.btngive=Button(topFrame, text='Give Book', font='arial 12 bold', padx=10)
+        self.btngive.pack(side=LEFT)
+
 
 
 def main():
@@ -60,7 +53,7 @@ def main():
     app = Main(root)
     root.title("Library Management System")
     root.geometry("1350x750+350+200")
-    # root.iconitmap("icons/icon.ico")
+    # root.iconitmap("icons/book.png")
     root.mainloop()
 
 
